092af3bc55ab1688086d8cf71db0d93a9db6e6f2
D.Zubkov <zda3171@gmail.com>
2022-05-19 19:56:10 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 10_000;
+        int onemile = 20;
+        int bonusmile = ticket / onemile;
+        System.out.println("Итого накопленных миль: " + bonusmile);
+    }
+}
